86aef39294458ecf788420b9cd22012f8d75cff9
Constantinos Christofilos <k.christofilos@thebeat.co>
2019-08-20 12:34:03 +0300

    Remove PHPStorm generated annotations

--- tests/data/config.php
+++ tests/data/config.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: konstantinoschristofilos
- * Date: 2019-03-29
- * Time: 11:31
- */
 
 return [
     'foo' => [
--- tests/data/invalid_config.php
+++ tests/data/invalid_config.php
@@ -1,9 +1,3 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: konstantinoschristofilos
- * Date: 2019-03-29
- * Time: 11:31
- */
 
 return new stdClass();
